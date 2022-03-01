fe0c65c721b17a4907174e07180f698847ee09ae
iamvigneshwars <iamvigneshwars@gmail.com>
2022-03-01 18:52:24 +0000

    Agent class

--- agent.py
+++ agent.py
@@ -1,14 +1,81 @@
+
 import gym 
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import namedtuple, deque
+from itertools import count
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
 import time
-env = gym.make("CartPole-v1")
-observation = env.reset()
-for _ in range(1000):
-    env.render()
-    action = env.action_space.sample()
-    obsertvation, reward, done, info = env.step(action)
-    time.sleep(0.01)
 
-    if done:
-        observation = env.reset()
 
-env.close
+# DQN Model
+class DQN(nn.Module):
+    def __init__(self, num_state_features):
+        super().__init__()
+         
+        self.fc1 = nn.Linear(in_features=num_state_features, out_features=100)   
+        self.fc2 = nn.Linear(in_features=100, out_features=50)
+        self.out = nn.Linear(in_features=50, out_features=2)            
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.out(x)
+        return x
+
+# Replay Memory
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.memory = deque([],maxlen=capacity)
+
+    def push(self, *args):
+    
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+
+# Epsilon Strategy
+class EpsilonGreedyStrategy():
+    def __init__(self, start, end , decay):
+        self.start = start
+        self.end = end
+        self.decay = decay
+
+    def get_exploration_rate(self, current_step):
+        return self.end + (self.start - self.end) * math.exp(-1 * current_step * self.decay)
+
+# Agent
+class Agent():
+    def __init__(self, strategy, num_actions, device):
+        self.current_step = 0
+        self.strategy = strategy
+        self.num_actions = num_actions
+        self.device = device
+
+    def select_action(self, state, policy_net):
+        rate = strategy.get_exploration_rate(self.current_step)
+        self.current_step += 1
+
+        # Random action (Explore)
+        if rate > random.random():
+            action = random.randrange(self.num_actions)
+            return torch.tensor([action]).to(self.device)
+        else:
+            # Predict the best action 
+            with torch.no_grad():
+                return policy_net(state).unsqueeze(dim=0).argmax(dim=1).to(self.device)
+
+
+# To store transitions
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
