78d2a8e777870fa6894189895a2010d303fac706
iamvigneshwars <iamvigneshwars@gmail.com>
2022-03-01 22:28:43 +0000

    agent play

--- agent.py
+++ agent.py
@@ -10,6 +10,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import time
+import sys
 
 
 # DQN Model
@@ -44,7 +45,7 @@ class ReplayMemory(object):
         return len(self.memory)
 
 
-# Epsilon Strategy
+# Greedy Epsilon Strategy
 class EpsilonGreedyStrategy():
     def __init__(self, start, end , decay):
         self.start = start
@@ -54,27 +55,8 @@ class EpsilonGreedyStrategy():
     def get_exploration_rate(self, current_step):
         return self.end + (self.start - self.end) * math.exp(-1 * current_step * self.decay)
 
-# Agent
-class Agent():
-    def __init__(self, strategy, num_actions, device):
-        self.current_step = 0
-        self.strategy = strategy
-        self.num_actions = num_actions
-        self.device = device
 
-    def select_action(self, state, policy_net):
-        rate = strategy.get_exploration_rate(self.current_step)
-        self.current_step += 1
-
-        # Random action (Explore)
-        if rate > random.random():
-            action = random.randrange(self.num_actions)
-            return torch.tensor([action]).to(self.device)
-        else:
-            # Predict the best action 
-            with torch.no_grad():
-                return policy_net(state).unsqueeze(dim=0).argmax(dim=1).to(self.device)
-
+# Takes care of game environment
 class GameManager():
     def __init__(self, device, environment):
         self.device = device
@@ -109,6 +91,26 @@ class GameManager():
         else:
             return torch.tensor(self.current_state, device = device).float()
 
+# Agent
+class Agent():
+    def __init__(self, strategy, num_actions, device):
+        self.current_step = 0
+        self.strategy = strategy
+        self.num_actions = num_actions
+        self.device = device
+
+    def select_action(self, state, policy_net):
+        rate = strategy.get_exploration_rate(self.current_step)
+        self.current_step += 1
+
+        # Random action (Explore)
+        if rate > random.random():
+            action = random.randrange(self.num_actions)
+            return torch.tensor([action]).to(self.device)
+        else:
+            # Predict the best action 
+            with torch.no_grad():
+                return policy_net(state).unsqueeze(dim=0).argmax(dim=1).to(self.device)
 
 def get_moving_average(period, values):
     values = torch.tensor(values, dtype=torch.float)
@@ -134,18 +136,18 @@ def plot(values, moving_avg_period):
     plt.pause(0.001)
     print("Episode", len(values), "\n", moving_avg_period, "episode moving avg:", moving_avg[-1])
  
+
+# To datastructure to store transitions
 Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward', 'mask'))
 
-
-batch_size = 1024
-gamma = 0.8
+batch_size = 256
+gamma = 0.999
 eps_start = 1
 eps_end = 0.01
 eps_decay = 0.001
-target_update = 10
-memory_size = 10000
+target_update = 50
+memory_size = 100000
 lr = 0.001
-num_episodes = 250
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 gm= GameManager(device, 'CartPole-v1')
@@ -155,56 +157,100 @@ memory = ReplayMemory(memory_size)
 
 policy_net = DQN(gm.num_states(), gm.num_actions()).to(device)
 target_net = DQN(gm.num_states(), gm.num_actions()).to(device)
+# Do not caclucate gradients on the target network 
 target_net.eval()
 optimizer = optim.Adam(params = policy_net.parameters(), lr=lr)
 
-episode_durations = []
 
-for episode in range(num_episodes):
-    gm.reset()
-    state = gm.get_state()
+def train(num_episodes):
 
-    for timestep in count():
-        gm.render()
-        action = agent.select_action(state, policy_net)
-        reward = gm.take_action(action)
-        next_state = gm.get_state()
-        mask = torch.tensor([0 if gm.done else 1], device = device)
-        # mask = 0 if gm.done else 1
-        memory.push(state, action, next_state, reward, mask)
-        state = next_state
+    episode_durations = []
 
-        if len(memory) >= batch_size:
-            transitions = memory.sample(batch_size)
-            batch = Transition(*zip(*transitions))
+    for episode in range(int(num_episodes)):
+        gm.reset()
+        state = gm.get_state()
 
-            states = torch.stack(batch.state)
-            actions = torch.cat(batch.action)
-            rewards = torch.cat(batch.reward)
-            next_states = torch.stack(batch.next_state)
-            masks = torch.cat(batch.mask)
+        for timestep in count():
+            gm.render()
+            # Action taken by Agent
+            action = agent.select_action(state, policy_net)
+            # Reward of the the action taken by the agent
+            reward = gm.take_action(action)
+            next_state = gm.get_state()
+            # This will be used to mask the target qvalues for the 
+            # terminal states (state after an episode has ended).
+            mask = torch.tensor([0 if gm.done else 1], device = device)
+            memory.push(state, action, next_state, reward, mask)
+            state = next_state
 
-            # Calculate current qvalue
-            current_qvalues = policy_net(states).gather(dim = 1, index = actions.unsqueeze(-1))
-            next_qvalues = target_net(next_states).max(dim = 1)[0].detach()
-            target_qvalues = (next_qvalues * mask * gamma) + rewards
+            if len(memory) >= batch_size:
+                # Sample a batch of experiences from the memory.
+                transitions = memory.sample(batch_size)
+                batch = Transition(*zip(*transitions))
 
-            loss = F.mse_loss(current_qvalues, target_qvalues)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+                states = torch.stack(batch.state)
+                actions = torch.cat(batch.action)
+                rewards = torch.cat(batch.reward)
+                next_states = torch.stack(batch.next_state)
+                masks = torch.cat(batch.mask)
 
-        if gm.done:
-            episode_durations.append(timestep)
-            plot(episode_durations, 100)
+                # Calculate current qvalue
+                current_qvalues = policy_net(states).gather(dim = 1, index = actions.unsqueeze(-1))
+                next_qvalues = target_net(next_states).max(dim = 1)[0].detach()
+                # Mask the target qvalues for the states that belongs to terminal states.
+                target_qvalues = (next_qvalues * masks * gamma) + rewards
+
+                loss = F.mse_loss(current_qvalues, target_qvalues.unsqueeze(1))
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+            if gm.done:
+                episode_durations.append(timestep)
+                plot(episode_durations, 100)
+                break
+
+        # Target network update.
+        if episode % target_update == 0:
+            target_net.load_state_dict(policy_net.state_dict())
+
+        if get_moving_average(100, episode_durations)[-1] >= 195:
             break
 
-    if episode % target_update == 0:
-        target_net.load_state_dict(policy_net.state_dict())
+    torch.save(policy_net.state_dict(), 'policy_net.pb')
+    print("*****Model Saved*****")
 
-    if get_moving_average(100, episode_durations)[-1] >= 195:
-        break
+    plt.show()
+    gm.close()
 
-gm.close()
     
+
+def play(num_episodes):
+    env = gym.make('CartPole-v1')
+    state = env.reset()
+    policy_net.load_state_dict(torch.load('policy_net.pb'))
     
+    for _ in range(int(num_episodes)):
+        print("*** Game", _ + 1, "***")
+        state = env.reset()
+        done = False
+        for _ in range(100000):
+            with torch.no_grad():
+                action = policy_net(torch.tensor(state, device = device)).argmax(dim=0).item()
+            env.render()
+            time.sleep(0.01)
+            new_state, reward, done, _ = env.step(action)
+            state = new_state
+
+            if done == True:
+                env.reset()
+                break
+
+    env.close()
+
+if __name__ == "__main__":
+    if (len(sys.argv) > 0 and sys.argv[1] == 'train'):
+        train(sys.argv[2])
+
+    if (len(sys.argv) > 0 and sys.argv[1] == 'play'):
+        play(sys.argv[2])
